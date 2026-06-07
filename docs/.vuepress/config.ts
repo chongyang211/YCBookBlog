497d6019cbfeef14bb9bee23165013df20b99418
yangchong <yangchong211@163.com>
2026-06-07 09:37:17 +0800

    fix: Git应用 permalink 更新为 00.README.md 的新值

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -138,7 +138,7 @@ export default defineConfig4CustomTheme<VdoingThemeConfig>({
           { text: 'GitHub技巧', link: '/pages/4c778760be26d8b3/' },
           { text: 'Nodejs', link: '/pages/117708e0af7f0bd9/' },
           { text: '博客搭建', link: '/pages/41f87d890d0a02af/' },
-          { text: 'Git应用', link: '/pages/89c2da/' },
+          { text: 'Git应用', link: '/pages/36ec62/' },
           { text: '技术模版', link: '/pages/tech-templates/' },
           { text: '技术规范', link: '/pages/code-style-guides/' },
           { text: 'Markdown', link: '/pages/markdown-basics/' },
