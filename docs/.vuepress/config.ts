9c4f413eb0f0f196d87797b2ba981518f8269b73
yangchong <yangchong211@163.com>
2026-06-07 09:04:02 +0800

    refactor: Git从入门精通 → Git应用

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -138,7 +138,7 @@ export default defineConfig4CustomTheme<VdoingThemeConfig>({
           { text: 'GitHub技巧', link: '/pages/4c778760be26d8b3/' },
           { text: 'Nodejs', link: '/pages/117708e0af7f0bd9/' },
           { text: '博客搭建', link: '/pages/41f87d890d0a02af/' },
-          { text: 'Git从入门精通', link: '/pages/89c2da/' },
+          { text: 'Git应用', link: '/pages/89c2da/' },
           { text: '技术模版', link: '/pages/tech-templates/' },
           { text: '技术规范', link: '/pages/code-style-guides/' },
         ],
