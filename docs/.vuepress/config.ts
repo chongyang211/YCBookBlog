90d2dc1799584a23db146c77c4bf1da6244eddb4
yangchong <yangchong211@163.com>
2026-06-07 08:50:57 +0800

    fix: 技术模版用permalink，修复404

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -139,7 +139,7 @@ export default defineConfig4CustomTheme<VdoingThemeConfig>({
           { text: 'Nodejs', link: '/pages/117708e0af7f0bd9/' },
           { text: '博客搭建', link: '/pages/41f87d890d0a02af/' },
           { text: 'Git从入门精通', link: '/pages/89c2da/' },
-          { text: '技术模版', link: '/03.技术/技术模版/' },
+          { text: '技术模版', link: '/pages/tech-templates/' },
         ],
       },
       {
---
+++ docs/03.技术/技术模版/README.md
@@ -0,0 +1,16 @@
+---
+title: 技术模版
+date: 2025-02-22 21:41:39
+permalink: /pages/tech-templates/
+sidebar: auto
+article: false
+comment: false
+editLink: false
+author:
+  name: 杨充
+  link: https://github.com/yangchong211
+---
+
+# 技术模版
+
+技术方案设计、复盘、分享的标准化模版集合。
