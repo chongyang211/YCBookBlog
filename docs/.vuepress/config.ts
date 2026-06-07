fbe3025e25e6c69e429c6bb03fcfcc8443b27a21
yangchong <yangchong211@163.com>
2026-06-07 08:57:50 +0800

    feat: 添加技术规范（C++/Java/JS/TS/Python/Go/Kotlin/Swift/Rust/Shell/Git）

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -140,6 +140,7 @@ export default defineConfig4CustomTheme<VdoingThemeConfig>({
           { text: '博客搭建', link: '/pages/41f87d890d0a02af/' },
           { text: 'Git从入门精通', link: '/pages/89c2da/' },
           { text: '技术模版', link: '/pages/tech-templates/' },
+          { text: '技术规范', link: '/pages/code-style-guides/' },
         ],
       },
       {
---
+++ docs/03.技术/技术规范/README.md
@@ -0,0 +1,16 @@
+---
+title: 技术规范
+date: 2025-06-06 10:00:00
+permalink: /pages/code-style-guides/
+sidebar: auto
+article: false
+comment: false
+editLink: false
+author:
+  name: 杨充
+  link: https://github.com/yangchong211
+---
+
+# 技术规范
+
+主流编程语言代码规范与 Git 提交规范合集。
