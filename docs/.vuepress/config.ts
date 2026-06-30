0d0ddf2d7c36fc16207650d69a9baf491ca15805
yangchong <yangchong211@163.com>
2026-06-30 11:29:13 +0800

    doc：认知跃迁总结 优化

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -277,7 +277,7 @@ export default defineConfig4CustomTheme<VdoingThemeConfig>({
     footer: {
       createYear: 2019, // 博客创建年份
       copyrightInfo:
-        '杨充 | <a href="https://github.com/杨充/vuepress-theme-vdoing/blob/master/LICENSE" target="_blank">MIT License</a> | <a href="https://beian.miit.gov.cn/" target="_blank">鄂ICP备2024073355号-1</a> | <img src="/img/beian.png" style="width: 15px; margin-bottom: -3px;" /> <a href="https://beian.mps.gov.cn/#/query/webSearch?code=45142202000030" rel="noreferrer" target="_blank">鄂ICP备2024073355号</a>', // 博客版权信息、备案信息等，支持a标签或换行标签</br>
+        '杨充 | <a href="https://yccoding.com/vuepress-theme-vdoing/blob/master/LICENSE" target="_blank">MIT License</a> | <a href="https://beian.miit.gov.cn/" target="_blank">鄂ICP备2024073355号-1</a> | <img src="/img/beian.png" style="width: 15px; margin-bottom: -3px;" /> <a href="https://beian.mps.gov.cn/#/query/webSearch?code=45142202000030" rel="noreferrer" target="_blank">鄂ICP备2024073355号</a>', // 博客版权信息、备案信息等，支持a标签或换行标签</br>
     },
 
     // 扩展自动生成frontmatter。（当md文件的frontmatter不存在相应的字段时将自动添加。不会覆盖已有的数据。）
