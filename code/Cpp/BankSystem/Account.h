e3636ee13aa2f74ed1acc98bf004bde91f01c584
yangchong <yangchong211@163.com>
2026-09-16 22:15:00 +0800

    Account 是抽象基类，不能直接实例化

--- code/Cpp/BankSystem/Account.h
+++ code/Cpp/BankSystem/Account.h
@@ -18,7 +18,8 @@ public:
     Account(const std::string& id, const std::string& name, double initBalance);
     // 析构函数，为什么要设计成 virtual 虚函数？如果不写成虚函数，子类delete时候，那么就只调基类析构，无法释放子类自己
     virtual ~Account() = default;
-    // 定义虚函数，子类必须实现
+    // 纯虚函数，子类必须实现
+    // 有纯虚函数的类，不可以直接new
     virtual void showInfo() const = 0;
     virtual bool withdraw(double amount) = 0;
     virtual char typeTag() const = 0;
