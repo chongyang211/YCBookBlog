a1df1c6c37b9f0c4f8900de8f502a77cb3723d21
yangchong <yangchong211@163.com>
2026-09-17 22:04:20 +0800

    看到这行字 = 类的连接关系正确

--- code/Cpp/BankSystem/Bank.h
+++ code/Cpp/BankSystem/Bank.h
@@ -11,11 +11,16 @@
 
 class Bank {
 private:
+    // 唯一的"数据库"
     std::vector<Account*> accounts;
+    // file文件
     std::string dataFile = "bank";
+    // 查找
     Account* findAccount(const std::string& id);
 public:
+    // 构造函数
     Bank();
+    // 析构函数
     ~Bank();
     void openAccount();
     void deposit();
